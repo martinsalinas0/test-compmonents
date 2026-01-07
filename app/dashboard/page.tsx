7b6475daf331785845624781438442375227d73a
martin <martinlsalinas@pm.me>
2026-01-07 00:26:22 -0600

    new

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 const DashboardHomePage = () => {
-  return <div className="p-6">DASHBOARD MAIN PAGE</div>;
+  return (
+    <div className="p-6">
+      DASHBOARD MAIN PAGE
+      <h1>hi</h1>
+    </div>
+  );
 };
 
 export default DashboardHomePage;
