a1b0f24f5efb6b17c0b00004feea59febcec97d8
martinsalinas0 <martinlsalinas@pm.me>
2026-01-19 16:59:53 -0600

    add ADMIN button on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="flex justify-center m-4 p-4">
+      HomePage
+      <button className="bg-amber-200 rounded-3xl p-3 m-3 text-9xl">
+        <Link href="/admin/">Admins</Link>
+      </button>
+    </div>
+  );
 };
 
 export default HomePage;
