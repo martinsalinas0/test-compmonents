bce5587e586e5f122f650c11033609153a245735
martin <martinlsalinas@pm.me>
2026-01-22 00:46:52 -0600

    add: create the dropdown menu for status change

--- app/(dashboard)/admin/list/jobs/active/page.tsx
+++ app/(dashboard)/admin/list/jobs/active/page.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import StatusChangeDDM from "@/components/dropdown-menus/status-change";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const JobsActivePage = () => {
-  return <div>JobsActivePage</div>;
+  return (
+    <div>
+      JobsActivePage
+      <div>
+        <StatusChangeDDM />
+      </div>
+    </div>
+  );
 };
 
 export default JobsActivePage;
---
+++ components/dropdown-menus/status-change.tsx
@@ -0,0 +1,40 @@
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+const StatusChangeDDM = () => {
+  return (
+    <div>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline">Status</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuGroup>
+            <DropdownMenuLabel>STATUS</DropdownMenuLabel>
+            <DropdownMenuItem>Open</DropdownMenuItem>
+            <DropdownMenuItem>Needs Quote</DropdownMenuItem>
+            <DropdownMenuItem>Quote Pending</DropdownMenuItem>
+            <DropdownMenuItem>Quote Rejected</DropdownMenuItem>
+            <DropdownMenuItem>Approved</DropdownMenuItem>
+            <DropdownMenuItem>In Progress</DropdownMenuItem>
+            <DropdownMenuItem>Completed</DropdownMenuItem>
+            <DropdownMenuItem>Paid</DropdownMenuItem>
+            <DropdownMenuItem>Cancelled</DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
+
+//'open', 'needs_quote', 'quote_pending', 'quote_rejected', 'approved', 'in_progress', 'completed', 'paid', 'cancelled');
+
+export default StatusChangeDDM;
