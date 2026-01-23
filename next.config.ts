6341805dae76b8f1bb2eec4d4f0a9c51703e0cc6
martinsalinas0 <martinlsalinas@pm.me>
2026-01-23 01:35:08 -0600

    udpate: next.config.js to new Image config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* other config options here */
   images: {
-    domains: ["randomuser.me"], // ✅ allow external images from this domain
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+      },
+    ],
   },
 };
 
