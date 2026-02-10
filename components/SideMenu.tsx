73e8e8f1b9ad23cd61b079e623347aa43f310c99
martinsalinas0 <martinlsalinas@pm.me>
2026-02-09 20:30:50 -0600

    remove prrojexts tab from side bar

--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -13,7 +13,7 @@ import {
 } from "lucide-react";
 
 import { NavMain } from "@/components/nav-main";
-import { NavProjects } from "@/components/nav-projects";
+
 import { NavUser } from "@/components/nav-user";
 import { CompanyHeader } from "@/components/CompanyHeader";
 import {
@@ -140,28 +140,12 @@ const data = {
         },
         {
           title: "Limits",
-          url: "/admin/settings",
+          url: "#",
         },
       ],
     },
   ],
-  projects: [
-    {
-      name: "Design Engineering",
-      url: "#",
-      icon: Frame,
-    },
-    {
-      name: "Sales & Marketing",
-      url: "#",
-      icon: PieChart,
-    },
-    {
-      name: "Travel",
-      url: "#",
-      icon: Map,
-    },
-  ],
+  
 };
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
@@ -172,7 +156,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       </SidebarHeader>
       <SidebarContent>
         <NavMain items={data.navMain} />
-        <NavProjects projects={data.projects} />
+        
       </SidebarContent>
       <SidebarFooter>
         <NavUser user={data.user} />
